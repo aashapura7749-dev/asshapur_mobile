9aa7fb2a977f2043e89c9dbffaf0f55bd4b08dac
aashapura7749-dev <aashapura7749@gmail.com>
2026-05-08 10:50:31 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function buy(model){
-  window.open("https://wa.me/919598919992?text=I want "+model);
-}
+document.getElementById("form").addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+
+    let message =
+    `Hello, my name is ${name}. My number is ${phone}. I want a website demo.`;
+
+    let url =
+    `https://wa.me/919598919992?text=${encodeURIComponent(message)}`;
+
+    window.open(url, "_blank");
+
+});
